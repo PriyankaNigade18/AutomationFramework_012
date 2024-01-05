6aae6d86bf5a41e068917366ae2a8aa67e8d36cd
PriyankaNigade18 <Piyu1818@gmail.com>
2024-01-05 22:23:23 +0530

    newline added

--- src/test/java/com/SwagLabs/TestCases/SampleTest.java
+++ src/test/java/com/SwagLabs/TestCases/SampleTest.java
@@ -4,7 +4,8 @@ public class SampleTest {
 
 	public static void main(String[] args) {
 
-System.out.println("This is for Git integration with jenkins");
+   System.out.println("This is for Git integration with jenkins");
+   System.out.println("New line added for testing");
 	}
 
 }
