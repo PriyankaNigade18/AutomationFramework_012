7a22f4ded3b2062038ec577c5fb9c42c10ac366e
PriyankaNigade18 <Piyu1818@gmail.com>
2024-01-05 22:19:24 +0530

    05-01-2024 GitJenkins

---
+++ src/test/java/com/SwagLabs/TestCases/SampleTest.java
@@ -0,0 +1,10 @@
+package com.SwagLabs.TestCases;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+
+System.out.println("This is for Git integration with jenkins");
+	}
+
+}
